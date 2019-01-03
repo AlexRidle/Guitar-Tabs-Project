33bd0198e94c18fb4b1964bfac1611299815fcc8
Alexander <AlexanderBely98@gmail.com>
2019-01-04 02:02:39 +0300

    commentConverter edit

--- src/main/java/com/project/MyProject/converter/CommentConverter.java
+++ src/main/java/com/project/MyProject/converter/CommentConverter.java
@@ -2,11 +2,19 @@ package com.project.MyProject.converter;
 
 import com.project.MyProject.dto.CommentDto;
 import com.project.MyProject.entity.Comment;
+import com.project.MyProject.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentConverter implements DtoEntityConverter<CommentDto, Comment> {
+
+    private final UserRepository userRepository;
+
+    public CommentConverter(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public CommentDto convertToDto(final Comment entity) {
@@ -21,8 +29,11 @@ public class CommentConverter implements DtoEntityConverter<CommentDto, Comment>
     @Override
     public Comment convertToDbo(final CommentDto commentDto) {
         final Comment comment = new Comment();
-        //get user id from authorization
         BeanUtils.copyProperties(commentDto, comment);
+        comment.setUser(userRepository.findByUsername(SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName()));
         return comment;
     }
 }
